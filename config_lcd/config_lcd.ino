a13407def70f3fb802b64ebf049b5e4b2d3c2208
Ishanka D Senevirathne <33729848+IshankaDSenevirathne@users.noreply.github.com>
2019-06-05 16:35:57 +0530

    navigation menu v1.1

--- config_lcd/config_lcd.ino
+++ config_lcd/config_lcd.ino
@@ -10,21 +10,21 @@
 
 int readkeypad(){
       int adc_key_in = analogRead(0); 
-      int ret = btnUNKNOWN;
+      int ret_VALUE = btnUNKNOWN;
 
-      if (adc_key_in < 50) ret = btnRIGHT;
-      if ((adc_key_in > 500) && (adc_key_in < 1150)) ret = btnNONE;
-      if ( (adc_key_in > 120) && (adc_key_in < 150) ) ret = btnUP;
-      if ( (adc_key_in > 250) && (adc_key_in < 350) ) ret = btnDOWN;
-      if ( (adc_key_in > 450) && (adc_key_in < 500) ) ret = btnLEFT;
-      if ( (adc_key_in > 700) && (adc_key_in < 750) ) ret = btnSELECT;
+      if (adc_key_in < 50) ret_VALUE = btnRIGHT;
+      if ((adc_key_in > 500) && (adc_key_in < 1150)) ret_VALUE = btnNONE;
+      if ( (adc_key_in > 120) && (adc_key_in < 150) ) ret_VALUE = btnUP;
+      if ( (adc_key_in > 250) && (adc_key_in < 350) ) ret_VALUE = btnDOWN;
+      if ( (adc_key_in > 450) && (adc_key_in < 500) ) ret_VALUE = btnLEFT;
+      if ( (adc_key_in > 700) && (adc_key_in < 750) ) ret_VALUE = btnSELECT;
 
-      return ret;
+      return ret_VALUE;
       }
 
 
 
-const char *button[7] ={" Fan             ", " Lamp           ", " Door Lock      ", " Security System", "", "", "??????"};
+char *button[] ={" Fan             ", " Lamp           ", " Door Lock      ", " Security System", "", "", "??????"};
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
@@ -36,10 +36,37 @@ void loop(){
       lcd.setCursor(0, 0);
       lcd.print("Select Option:");
       lcd.setCursor(0,1);
-      lcd.print(button[readkeypad()]);
-      delay(400);
-      if(readkeypad()==4){
-        lcd.print("Turn ON :D");
+      int option=readkeypad();
+      switch(option){
+        case 0:
+          lcd.print(button[option]);
+          while(1){
+            option=readkeypad();
+            if(option==4){
+              lcd.setCursor(0,1);
+              lcd.print("SELECTED         ");
+              option=32;
+              delay(1000);
+              break;
+            }
+            }
+            if(option==32){
+              lcd.setCursor(0,1);
+              lcd.print("OPTIONS HERE    ");
+            }
+          break;
+        case 1:
+          lcd.print(button[option]);
+          break;
+        case 2:
+          lcd.print(button[option]);
+          break;
+        case 3:
+          lcd.print(button[option]);
+          break;
+        default :
+          lcd.print("");
       }
-      }
+      
+}
       
