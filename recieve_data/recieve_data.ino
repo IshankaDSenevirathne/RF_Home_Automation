b3b4d35f5ab0566cb64cabcac91dc364c8e57d9b
Ishanka D Senevirathne <33729848+IshankaDSenevirathne@users.noreply.github.com>
2019-06-19 09:56:58 +0530

    Tweaks for transmitter and reciver model

--- recieve_data/recieve_data.ino
+++ recieve_data/recieve_data.ino
@@ -5,18 +5,13 @@
 #define pirInputPin  8 
 #define fanInputPin  3
 #define lampInputPin  5
-#define doorlockInputPin  6
-#define securityInputPin  7
 #define servoPin 10
-#define irPin 12
+#define buzzerPin 12
 
-//byte temperature =0;
-//byte humidity = 0;
-//int dht_temp =0;
-//int temp_mapped;
 
 Servo myservo;  
 RCSwitch mySwitch = RCSwitch();
+
 
 int pinDHT11 = 4;
 SimpleDHT11 dht11(pinDHT11);
@@ -38,24 +33,32 @@ void init_LAMP_OFF(){
   digitalWrite(lampInputPin,LOW);
 }
 void init_DOORLOCK_ON(){
-  digitalWrite(doorlockInputPin,HIGH);
-  //servo control here
-  myservo.write(0); // sets the servo position to close the lock
-  delay(15); // waits for the servo to get there
+  myservo.write(80); // sets the servo position to close the lock
 }
 void init_DOORLOCK_OFF(){
-  digitalWrite(doorlockInputPin,LOW);
-  myservo.write(90); // sets the servo position to open the lock
-  delay(15); // waits for the servo to get there
+//  digitalWrite(doorlockInputPin,LOW);
+  pirVal = digitalRead(pirInputPin);
+  if(pirVal==LOW){
+  myservo.write(80); 
+  }
+  if(pirVal==HIGH){
+  myservo.write(0);
+  }
 }
 void init_SECURITY_ON(){
-  digitalWrite(7,HIGH);
   init_DOORLOCK_ON();
+  pirVal = digitalRead(pirInputPin);
+  if(pirVal==HIGH){
+    digitalWrite(buzzerPin,HIGH);
+    delay(100);
+    digitalWrite(buzzerPin,LOW);
+  }else if(pirVal==LOW){
+    digitalWrite(buzzerPin,LOW);  
+  }
   //alarm settings here
-  
 }
 void init_SECURITY_OFF(){
-  digitalWrite(securityInputPin,LOW);
+//  digitalWrite(securityInputPin,LOW);
   //alarm settings here
 }
 void control_FLOW(){
@@ -77,7 +80,7 @@ void control_FLOW(){
               recieved_DATA=char(mySwitch.getReceivedValue());
               int err = SimpleDHTErrSuccess;
               if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
-                Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(500);
+                Serial.print("Read DHT11 failed, err="); Serial.println(err);
                 return;
               }
               
@@ -100,33 +103,21 @@ void control_FLOW(){
     else if(recieved_DATA=='e'){
       init_LAMP_OFF();
     }
-    if(recieved_DATA=='f'){
+    else if(recieved_DATA=='f'){
       //constantly check for motion
-      while(recieved_DATA!='d'||recieved_DATA!='e'){
         pirVal = digitalRead(pirInputPin);  // read input value
         if (pirVal == HIGH) {            // check if the input is HIGH 
-            Serial.println("Motion detected!");
-            //pirState = HIGH;
+              Serial.println("Motion detected!");
+              pirState = HIGH;
               int ldr_val = analogRead(A0);
               Serial.println(ldr_val);
               int pwm_LAMP = map(ldr_val, 0, 850, 0,255);
-              Serial.println(pwm_LAMP);
+              //Serial.println(pwm_LAMP);
               analogWrite(lampInputPin, 255-pwm_LAMP);
-              delay(5);
         }
          else if(pirVal == LOW){
-          analogWrite(lampInputPin, 0);
-            
+          analogWrite(lampInputPin, 0); 
           }
-//        } else {
-//          if (pirState == HIGH){
-//            Serial.println("Motion ended!");
-//            digitalWrite(lampInputPin,LOW);
-//            pirState = LOW;
-//          }
-        }
-        mySwitch.resetAvailable();
-        control_FLOW();
       }
     
     else if(recieved_DATA=='g'){
@@ -134,9 +125,6 @@ void control_FLOW(){
     }
     else if(recieved_DATA=='h'){
       init_DOORLOCK_OFF();
-//      if(irInput==HIGH){
-//        init_DOORLOCK_ON();
-//      }
     }
     else if(recieved_DATA=='i'){
       init_SECURITY_ON();
@@ -151,18 +139,15 @@ void control_FLOW(){
 void setup() {
   Serial.begin(9600);
   pinMode(pirInputPin, INPUT);
-  pinMode(irPin,INPUT);
+  pinMode(buzzerPin,OUTPUT);
   pinMode(fanInputPin,OUTPUT);
   pinMode(lampInputPin,OUTPUT);
-  pinMode(doorlockInputPin,OUTPUT);
-  pinMode(securityInputPin,OUTPUT);
+//  pinMode(doorlockInputPin,OUTPUT);
+//  pinMode(securityInputPin,OUTPUT);
   myservo.attach(servoPin);  //Attach Servo to servoPin
   mySwitch.enableReceive(0);  // Receiver on interrupt 0 pin 2
 }
 
 void loop() {
   control_FLOW();
-
-  
-  delay(1500);
 }
