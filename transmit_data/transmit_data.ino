a56affd8275dbf2ca375b853ae0a14569e7d0399
mdkacer <56209657+mdkacer@users.noreply.github.com>
2020-10-02 05:18:59 +0530

    Update transmit_data.ino

--- transmit_data/transmit_data.ino
+++ transmit_data/transmit_data.ino
@@ -1,7 +1,7 @@
 #include <RCSwitch.h>
 
 RCSwitch mySwitch = RCSwitch();
-
+int result = 0;
 void setup() {
 
   Serial.begin(9600);
@@ -11,6 +11,12 @@ void setup() {
 }
 
 void loop() {
-  mySwitch.send('a', 24);
-  delay(1000);    
+  result = mySwitch.send('a', 24);
+  delay(1000);   
+  if(result == 0){
+    Serial.println("Transmit succesfully");
+  }else{
+    Serial.printin("Something wrong");
+  }
+  
 }
